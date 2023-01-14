2b6972a85154cbddea7c69020533e244a43796f3
bswkim <121818631+bswkim@users.noreply.github.com>
2023-01-14 03:32:27 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import OnboardingScreen from './screens/OnBoardingScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const AppStack = createStackNavigator();
+
+const App = () => {
+
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator headerMode = "none">
+                <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
+                <AppStack.Screen name="Login" component={LoginScreen}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App();
+
