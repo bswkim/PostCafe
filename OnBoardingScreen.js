3e0fde35ae0867f6271a21b65c3f3a19d06f3598
bswkim <121818631+bswkim@users.noreply.github.com>
2023-01-14 04:59:06 -0800

    Add files via upload

--- OnBoardingScreen.js
+++ OnBoardingScreen.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { View, Text, Button, StyleSheet} from 'react-native';
 import Onboarding from "react-native-onboarding-swiper";
 
+const Dots = () =>{
+    return (
+        <View
+            style={{
+                width:5,
+                height:5,
+            }}
+        />
+    );
+};
+
+const Skip = ({...props}) => (
+    <TouchableOpacity {...props} ><Text>Skip</Text></TouchableOpacity>
+);
+const Next = ({...props}) => (
+    <TouchableOpacity {...props}><Text>Next</Text></TouchableOpacity>
+);
+const Done = ({...props}) => (
+    <TouchableOpacity {...props}><Text>Done</Text></TouchableOpacity>
+);
+
 const OnBoardingScreen = ({navigation}) => {
     return(
-        <Onboarding 
+        <Onboarding
+        SkipButtonComponent={Skip} 
+        NextButtonComponent={Next}
+        DoneButtonComponent={Done}
         onSkip={() => navigation.navigate("Login")}
         onDone={() => navigation.navigate("Login")}
         pages = {[
