2e80bc15c8a2c75c2d470b0325e348451b0c1ac8
bswkim <121818631+bswkim@users.noreply.github.com>
2023-01-13 17:56:18 -0800

    Create ProfileScreen.js

---
+++ ProfileScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from 'react-native-paper';
+
+const ProfileScreen = () => {
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.userInfoSection}>
+        <View style={{flexDirection: 'row', marginTop: 15}}>
+          <Avatar.Image 
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
+            }}
+            size={80}
+          />
+          <View style={{marginLeft: 20}}>
+            <Title style={[styles.title, {
+              marginTop:15,
+              marginBottom: 5,
+            }]}>John Doe</Title>
+            <Caption style={styles.caption}>@j_doe</Caption>
+          </View>
+        </View>
+      </View>
+        </SafeAreaView>
+    );
+}
