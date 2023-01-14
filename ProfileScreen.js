e9122ce078577606adb0a774ed4338237617ea14
John5675 <79059047+John5675@users.noreply.github.com>
2023-01-13 18:58:24 -0800

    Add files via upload

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import {View, SafeAreaView, StyleSheet} from 'react-native';
 import {
   Avatar,
   Title,
   Caption,
   Text,
   TouchableRipple,
-} from 'react-native-paper';
-
+} from "react-native-paper";
 const ProfileScreen = () => {
-    return(
-        <SafeAreaView style={styles.container}>
-            <View style={styles.userInfoSection}>
-        <View style={{flexDirection: 'row', marginTop: 15}}>
-          <Avatar.Image 
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userInfoSection}>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Avatar.Image
             source={{
               uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png",
             }}
             size={80}
           />
-          <View style={{marginLeft: 20}}>
-            <Title style={[styles.title, {
-              marginTop:15,
-              marginBottom: 5,
-            }]}>Brian</Title>
+          <View style={{ marginLeft: 20 }}>
+            <Title
+              style={[
+                styles.title,
+                {
+                  marginTop: 15,
+                  marginBottom: 5,
+                },
+              ]}
+            >
+              Brian
+            </Title>
             <Caption style={styles.caption}>@kmsnoo</Caption>
           </View>
         </View>
       </View>
-        </SafeAreaView>
-    );
-}
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
---
+++ coffee.js
@@ -0,0 +1,47 @@
+export const COFFEE = [
+  {
+    image:
+      "https://sandysbluehillcafe.com/wp-content/uploads/2018/05/coffee-gift-cards-300x300.jpg",
+    date: "January 13, 2022",
+  },
+  {
+    image:
+      "https://wholefoodsoulfoodkitchen.com/wp-content/uploads/2022/05/peanut-butter-coffee-3-300x300.jpg",
+    date: "January 14, 2022",
+  },
+  {
+    image:
+      "https://sanremouk.com/wp-content/uploads/2019/06/better-black-2-300x300.png",
+    date: "January 15, 2022",
+  },
+  {
+    image:
+      "https://nationaltoday.com/wp-content/uploads/2021/12/National-Gourmet-Coffee-Day-300x300.jpg",
+    date: "January 16, 2022",
+  },
+  {
+    image:
+      "https://www.funfoodfrolic.com/wp-content/uploads/2020/09/Cold-Coffee-Thumbnail-300x300.jpg",
+    date: "January 17, 2022",
+  },
+  {
+    image:
+      "https://thespicetrain.com/wp-content/uploads/2020/07/spiced-irish-coffee-20-1-0-300x300.jpg",
+    date: "January 18, 2022",
+  },
+  {
+    image:
+      "https://www.happyfoodstube.com/wp-content/uploads/2016/07/1-Minute-Instant-Iced-Coffee-Easy-Recipe-300x300-1.jpg",
+    date: "January 19, 2022",
+  },
+  {
+    image:
+      "https://stories.starbucks.com/uploads/2021/07/SBX20210707-ColdCoffees-Iced-Chocolate-Almondmilk-Shaken-Espresso-300x300.jpg",
+    date: "January 20, 2022",
+  },
+  {
+    image:
+      "https://avenueeastcobb.com/wp-content/uploads/2021/11/Untitled-300-x-300-px.png",
+    date: "January 21, 2022",
+  },
+];
