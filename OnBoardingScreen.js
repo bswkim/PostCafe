09381f5804678ef982ed6dd1397446a759f2a8d8
bswkim <121818631+bswkim@users.noreply.github.com>
2023-01-14 02:48:18 -0800

    Add files via upload

---
+++ OnBoardingScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, Button, StyleSheet} from 'react-native';
+import Onboarding from "react-native-onboarding-swiper";
+
+const OnBoardingScreen = ({navigation}) => {
+    return(
+        <Onboarding pages = {[
+            {
+                backgroundColor: '#fff',
+                title: 'Onboarding',
+                subtitle:'Done',
+            },
+            {
+                backgroundColor: '#fff',
+                title: 'Onboarding2'
+            }
+        ]}/>
+    );
+};
+
+export default OnBoardingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+});
+
